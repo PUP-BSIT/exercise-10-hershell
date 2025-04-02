e33f38ac621ca833e611d67aee28a83a9a988346
durante-stephanie <durantestephanie07@gmail.com>
2025-04-02 22:01:38 +0800

    add changes to js file (#2)
    * transfer comment to js file
    
    * update validate comment
    
    * update add comment
    
    * add update comments
    
    * add sorting comment
    
    * update js file
    
    * add changes in js file
    
    * add spaces for continuation line
    
    * fix format

--- durante_stephanie/index.js
+++ durante_stephanie/index.js
@@ -2,31 +2,82 @@ const commentName = document.getElementById("comment_name");
 const commentMessage = document.getElementById("comment_form");
 const commentButton = document.getElementById("comment_button");
 const commentContainer = document.getElementById("comment_list");
+const sortType = document.getElementById("sort_type");
+
+const commentData = [
+  {
+    name: "Florido",
+    comment: `Wow!`,
+    date: new Date("03/19/2025, 11:47:28 PM"),
+  },
+  {
+    name: "Siervo",
+    comment: `There's always a way for those goals!`,
+    date: new Date("03/19/2025, 11:50:34 PM"),
+  },
+  {
+    name: "Riomalos",
+    comment: `These goals are excellent and show that you’re planning
+          for a bright future filled with new experiences!`,
+    date: new Date("03/19/2025, 11:52:45 PM"),
+  },
+];
 
 function validateComment() {
-  if (commentName.value.length && commentMessage.value.length) {
-    commentButton.disabled = false;
-  } else {
-    commentButton.disabled = true;
-  }
+  commentButton.disabled = !(
+        commentName.value.trim() && commentMessage.value.trim());
 }
 
 function addComment() {
   const newComment = {
     name: commentName.value,
     comment: commentMessage.value,
+    date: new Date(),
   };
 
-  const newCommentElement = document.createElement("li");
-  newCommentElement.innerHTML = `${newComment.name}: ${newComment.comment}`;
+  commentData.push(newComment);
+  sortComments();
+}
 
-  commentContainer.appendChild(newCommentElement);
+function updateComments() {
+  commentContainer.innerHTML = "";
 
+  for (const comment of commentData) {
+    const newComment = document.createElement("li");
+    const commentDate = new Date(comment.date);
+
+    newComment.innerHTML = `
+          ${comment.name}: ${comment.comment}
+          (${commentDate.toLocaleString()})`;
+
+    commentContainer.append(newComment);
+  }
+}
+
+function sortComments() {
+  const ascendingSort = "ascending";
+
+  commentData.sort((a, b) => {
+    if (sortType.value == ascendingSort) {
+      return a.date - b.date;
+    } else {
+      return b.date - a.date;
+    }
+  });
+  
+  updateComments();
+}
+
+function clearElement() {
+  addComment();
   commentName.value = "";
   commentMessage.value = "";
-  validateComment();
+  commentButton.disabled = true;
 }
 
 commentName.addEventListener("input", validateComment);
 commentMessage.addEventListener("input", validateComment);
 commentButton.addEventListener("click", addComment);
+sortType.addEventListener("change", sortComments);
+
+sortComments();
