eed4082a6ae9802b67b850e56b59b8d0945d1c26
florido-maydelyn <maydelynflorido07@gmail.com>
2025-04-02 23:09:41 +0800

    make the comment sortable by date (#8)
    * add changes to addElement
    
    * add sortComments function
    
    * add displaayComments function
    
    * call functions inside addElement
    
    * add functionality to sort options and call function
    
    * fix format according to coding guidelines

--- florido_maydelyn/script.js
+++ florido_maydelyn/script.js
@@ -1,6 +1,27 @@
 let nameInput = document.getElementById("comment_name");
 let commentInput = document.getElementById("comment");
 let commentButton = document.getElementById("comment_button");
+let sortOrder = "ascending";
+
+let commentArray = [
+  {
+    name: "Durante",
+    comment: "I hope you reach all your goals!",
+    date: new Date("03/19/2025, 11:50:32 PM"),
+  },
+  {
+    name: "Riomalos",
+    comment:
+      "These goals are fantastic and show that you’re focused on "
+          + "achieving success!",
+    date: new Date("03/19/2025, 11:38:12 PM"),
+  },
+  {
+    name: "Siervo",
+    comment: "Take your time for your success.",
+    date: new Date("03/26/2025, 6:25:47 PM"),
+  },
+];
 
 function toggleButton() {
   commentButton.disabled = !(
@@ -9,16 +30,41 @@ function toggleButton() {
 }
 
 function addElement() {
-  let nameInput = document.getElementById("comment_name").value;
-  let commentInput = document.getElementById("comment").value;
+  let timestamp = new Date();
+  let comment = {
+    name: nameInput.value,
+    comment: commentInput.value,
+    date: timestamp,
+  };
 
-  let commentSection = document.querySelector(".groupmate-comments");
-  let commentHolder = document.createElement("p");
-  commentSection.append(commentHolder);
-  commentHolder.textContent = nameInput + ": " + commentInput;
+  commentArray.push(comment);
 
+  sortComments();
+  displayComments();
   clearInput();
   toggleButton();
+}
+
+function sortComments() {
+  commentArray.sort((a, b) => {
+    if (sortOrder === "ascending") {
+      return a.date - b.date;
+    } else {
+      return b.date - a.date;
+    }
+  });
+}
+
+function displayComments() {
+  let commentSection = document.querySelector("#groupmate-comments");
+  commentSection.innerHTML = "";
+
+  commentArray.forEach((comment) => {
+    let commentHolder = document.createElement("p");
+    commentHolder.innerHTML = `${comment.name}: ${comment.comment}
+        (${comment.date.toLocaleString()})`;
+    commentSection.append(commentHolder);
+  });
 }
 
 function clearInput() {
@@ -28,3 +74,14 @@ function clearInput() {
 
 nameInput.addEventListener("input", toggleButton);
 commentInput.addEventListener("input", toggleButton);
+
+let sortToggle = document.getElementById("sort_toggle");
+sortToggle.addEventListener("change", () => {
+  selectSort = sortToggle.value;
+  sortOrder = selectSort;
+  sortComments();
+  displayComments();
+});
+
+sortComments();
+displayComments();
