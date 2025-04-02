cc44fd8d6b21d958956c3f4212001fddff70d9fa
Riomalos-ZyrrahFeil <zriomalos@gmail.com>
2025-04-02 23:25:34 +0800

    make the comment sortable by date (#11)
    * add changes to appendComment
    
    * add function to sortComments
    
    * add displayComments function
    
    * add function to update sort order
    
    * fix format

--- riomalos_zyrrah/script.js
+++ riomalos_zyrrah/script.js
@@ -1,3 +1,7 @@
+let sortOrder = 'asc';
+let commentForm = document.querySelector(".comment-form form");
+let commentsSection = document.querySelector(".teammate-comment");
+
 function checkForm() {
   let name = document.getElementById("name_comment").value;
   let comment = document.getElementById("comment").value;
@@ -14,24 +18,60 @@ function checkForm() {
   }
 }
 
-let commentForm = document.querySelector(".comment-form form");
-let commentsSection = document.querySelector(".teammate-comment");
+let comments = [
+  {
+    text: "I hope you reach all your goals!",
+    author: "Durante",
+    date: new Date("3/19/2025, 11:57 PM"),
+  },
+  {
+    text: "Wow!",
+    author: "Florido",
+    date: new Date("3/19/2025, 11:34 PM")
+  },
+  {
+    text: "Keep up with your goals and this design!",
+    author: "Siervo",
+    date: new Date("3/19/2025, 11:42 PM"),
+  },
+];
 
 function appendComment(event) {
   event.preventDefault();
 
   let name = document.getElementById("name_comment").value;
   let comment = document.getElementById("comment").value;
+  let timestamp = new Date();
 
-  let newComment = document.createElement("p");
-  newComment.textContent = `${comment} -${name}`;
-
-  commentsSection.appendChild(newComment);
+  comments.push({ text: comment, author: name, date: timestamp });
 
   document.getElementById("name_comment").value = "";
   document.getElementById("comment").value = "";
 
   checkForm();
+  displayComments(sortComments(comments, sortOrder));
+}
+
+function sortComments(commentsArray, order = 'asc') {
+  return commentsArray.sort((a, b) => {
+    return order === 'asc' ? a.date - b.date : b.date - a.date;
+  });
+}
+
+function displayComments(commentsArray) {
+  commentsSection.innerHTML = "<h3>Comments</h3>";
+
+  commentsArray.forEach(comment => {
+    let newComment = document.createElement("p");
+    newComment.textContent = `${comment.text} - ${comment.author}
+        (${comment.date.toLocaleString()})`;
+    commentsSection.appendChild(newComment);
+  });
+}
+
+function changeSortOrder(order) {
+  sortOrder = order;
+  displayComments(sortComments(comments, sortOrder));
 }
 
 commentForm.addEventListener("submit", appendComment);
