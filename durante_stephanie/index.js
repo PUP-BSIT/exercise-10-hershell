48c0924d3baf1ad02773bd95b2e97413bec6deb0
durante-stephanie <durantestephanie07@gmail.com>
2025-04-02 16:44:19 +0800

    transfer comment to js file

--- durante_stephanie/index.js
+++ durante_stephanie/index.js
@@ -3,6 +3,25 @@ const commentMessage = document.getElementById("comment_form");
 const commentButton = document.getElementById("comment_button");
 const commentContainer = document.getElementById("comment_list");
 
+const commentData = [
+  {
+    name: "Florido",
+    comment: `Wow!`,
+    date: new Date("03/19/2025, 10:15:28 PM"),
+  },
+  {
+    name: "Siervo",
+    comment: `wow`,
+    date: new Date("03/19/2025, 10:20:34 PM"),
+  },
+  {
+    name: "Riomalos",
+    comment: `These goals are excellent and show that you’re planning 
+    for a bright future filled with new experiences!`,
+    date: new Date("03/19/2025, 10:33:45 PM"),
+  },
+];
+
 function validateComment() {
   if (commentName.value.length && commentMessage.value.length) {
     commentButton.disabled = false;
