641048dc37122a202dde500e2029902cd8abd210
florido-maydelyn <maydelynflorido07@gmail.com>
2025-04-02 20:01:30 +0800

    add changes in js file (#7)
    * update toggleButton function
    
    * update clearInput function
    
    * update js file

--- florido_maydelyn/script.js
+++ florido_maydelyn/script.js
@@ -1,13 +1,11 @@
-function toggleButton() {
-  let nameInput = document.getElementById("comment_name");
-  let commentInput = document.getElementById("comment");
-  let commentButton = document.getElementById("comment_button");
+let nameInput = document.getElementById("comment_name");
+let commentInput = document.getElementById("comment");
+let commentButton = document.getElementById("comment_button");
 
-  if (nameInput.value.length && commentInput.value.length) {
-    commentButton.disabled = false;
-  } else {
-    commentButton.disabled = true;
-  }
+function toggleButton() {
+  commentButton.disabled = !(
+    nameInput.value.trim() && commentInput.value.trim()
+  );
 }
 
 function addElement() {
@@ -24,13 +22,9 @@ function addElement() {
 }
 
 function clearInput() {
-  document.getElementById("comment_name").value = "";
-  document.getElementById("comment").value = "";
+  nameInput.value = "";
+  commentInput.value = "";
 }
 
-document
-  .getElementById("comment_name").addEventListener("input", toggleButton);
-document
-  .getElementById("comment").addEventListener("input", toggleButton);
-document
-  .getElementById("comment_button").addEventListener("click", addElement);
+nameInput.addEventListener("input", toggleButton);
+commentInput.addEventListener("input", toggleButton);
